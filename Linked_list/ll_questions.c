e88df92fc31248ad7982a264a891249c07dc329f
afzalwarsi <afzalwarsi628@gmail.com>
2025-11-23 16:05:48 +0530

    linked list Questions

---
+++ Linked_list/ll_questions.c
@@ -0,0 +1,31 @@
+// 1. Order you should master:
+
+// 2. Reverse LL (iterative + recursive)
+
+// 3. Find middle (slow/fast)
+
+// 4. Loop detect + loop remove
+
+// 5. Merge sorted lists
+
+// 6. Insert / Delete variations
+
+// 7. Remove Nth from end
+
+// 8. Palindrome check
+
+// 9. Reverse in groups of k
+
+// 10. Intersection of two lists
+
+// 11. Merge sort on list
+
+// 12. Add two numbers
+
+// 13. Rotate list
+
+// 14.Flatten list
+
+// 15. Clone list with random pointer
+
+//16.  Remove duplicates
